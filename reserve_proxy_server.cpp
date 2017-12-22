570ed3dd9d92e58ac8ffebc7e20929118232985a
absentje <absent37@list.ru>
2017-12-22 19:54:35 +0300

    Update reserve_proxy_server.cpp

--- reserve_proxy_server.cpp
+++ reserve_proxy_server.cpp
@@ -1,10 +1,17 @@
 #include "reserve_proxy_server.h"
+#include <iostream>
+#include <boost/bind.hpp>
 
-Server::Server(boost::asio::io_service& io_service, short local_port, const std::string remote_ip, short remote_port) :
-	_io_service(io_service), acceptor_(io_service, tcp::endpoint(tcp::v4(), local_port)),
-	_remote_ip(remote_ip), _remote_port(remote_port)
+using boost::asio::ip::tcp;
+using std::ofstream;
+using std::cerr;
+using std::endl;
+
+Server::Server(boost::asio::io_service& io_service, short local_port, const std::string remote_ip, short remote_port) 
+	:	_io_service(io_service), acceptor_(io_service, tcp::endpoint(tcp::v4(), local_port)),
+	_remote_ip(remote_ip), _remote_port(remote_port) 
 {
-	const char NAME_FILE[] = "file_log.txt";
+	static const char NAME_FILE[] = "file_log.txt";
 	file_log = boost::shared_ptr<ofstream>(new ofstream(NAME_FILE));
 	start_accept();
 }
@@ -13,7 +20,7 @@ boost::shared_ptr<UserSession> Server::create_session() {
 	return boost::shared_ptr<UserSession>(new UserSession(_io_service, file_log));
 }
 
-bool Server::start_accept() {
+bool Server::start_accept() noexcept {
 	try {
 		session = create_session();
 		acceptor_.async_accept(session->get_client_socket(),
@@ -21,18 +28,20 @@ bool Server::start_accept() {
 				boost::asio::placeholders::error));
 	}
 	catch (const std::exception& e) {
-		cerr << "acceptor exception: " << e.what() << std::endl;
+		cerr << "create_session exception: " << e.what() << endl;
 		return false;
+	}
+	catch (...) {
+		cerr << "another exception, need to check it." << endl;
 	}
 	return true;
 }
 
-void Server::handle_accept(const boost::system::error_code& error) {
+void Server::handle_accept(const boost::system::error_code& error) noexcept {
 	if (!error) {
-		cout << "handle_accept port_client: " << session->get_client_socket().remote_endpoint().port() << "\n\n"; // (label)
 		session->start(_remote_ip, _remote_port);
 		if (!start_accept()) {
-			std::cerr << "Failed to accept.\n\n";
+			cerr << "Failed to accept.\n\n";
 		}
 	}
 	else {
