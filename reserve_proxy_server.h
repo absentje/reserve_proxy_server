d6d4f9beb9406dcd61635c2c14029f7b03a05663
absentje <absent37@list.ru>
2017-12-22 19:54:58 +0300

    Update reserve_proxy_server.h

--- reserve_proxy_server.h
+++ reserve_proxy_server.h
@@ -2,32 +2,24 @@
 #define _RESERVE_PROXY_SERVER__
 
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include <string>
-#include <cstddef>
 #include <fstream>
-#include <cstring>
-#include <cstdlib>
-#include <cstdio>
+#include <exception>
 
 // BOOST includes
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 // include project-files
 #include "user_session.h"
 
-using namespace std;
-using boost::asio::ip::tcp;
-
-class Server {
+class Server final {
+	using tcp = boost::asio::ip::tcp;
 public:
 	Server(boost::asio::io_service& io_service, short local_port, const std::string remote_ip, short remote_port);
 	boost::shared_ptr<UserSession> create_session();
-	bool start_accept();
-	void handle_accept(const boost::system::error_code& error);
+	bool start_accept() noexcept;
+	void handle_accept(const boost::system::error_code& error) noexcept;
 
 private:
 	boost::asio::io_service& _io_service;
@@ -35,7 +27,7 @@ private:
 	boost::shared_ptr<UserSession> session;
 	std::string _remote_ip;
 	short _remote_port;
-	boost::shared_ptr<ofstream> file_log;
+	boost::shared_ptr<std::ofstream> file_log;
 };
 
 
