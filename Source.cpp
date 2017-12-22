82dc4d88d8421aa4ac888504688698a0fcf34ef6
absentje <absent37@list.ru>
2017-12-22 19:53:19 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,41 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <cstdlib>
-#include <cstdio>
 
 // BOOST includes
 #include <boost/asio.hpp>
 // include project-files
 #include "reserve_proxy_server.h"
 
-using namespace std;
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
 using boost::asio::ip::tcp;
-
-
 
 int main(int argc, char** argv) {
 	setlocale(LC_CTYPE, "Russian");
-	try
-	{
-		cout << "Введите порт открываемого прокси сервера :\n";
-		string port;
-		getline(cin, port);
-		cout << "Введите ip адрес сервера с БД :\n";
+	try {
+		short port, port_proxy;
 		string ip_proxy;
-		getline(cin, ip_proxy);
-		cout << "Введите порт сервера с БД :\n";
-		string port_proxy;
-		getline(cin, port_proxy);
+		{
+			// input parameters
+			cout << "Введите порт открываемого прокси сервера :\n";
+			cin >> port;
+			cout << "Введите ip адрес сервера с БД :\n";
+			cin >> ip_proxy;
+			cout << "Введите порт сервера с БД :\n";
+			cin >> port_proxy;
+		}
+
 		boost::asio::io_service io_service;
-		Server server(io_service, (short)atoi(port.c_str()), ip_proxy, (short)atoi(port_proxy.c_str()));
+		Server server(io_service, port, ip_proxy, port_proxy);
 		server.start_accept();
 		io_service.run();
 	}
 	catch (std::exception& e)
 	{
-		std::cerr << "Exception: " << e.what() << "\n";
+		cerr << "Exception: " << e.what() << "\n";
 		system("pause");
 	}
 	catch (...)
