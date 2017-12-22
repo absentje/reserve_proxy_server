5d72afbcdee70fedca8a2dcd1ac3db6b43e5bbf7
absentje <absent37@list.ru>
2017-12-22 19:53:41 +0300

    Update user_session.h

--- user_session.h
+++ user_session.h
@@ -2,46 +2,39 @@
 #define _USER_SESSION__
 
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
 #include <cstddef>
 #include <fstream>
-#include <cstring>
-#include <cstdlib>
-#include <cstdio>
+#include <string>
 
 // BOOST includes
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
-using namespace std;
-using boost::asio::ip::tcp;
 
+class UserSession final : public boost::enable_shared_from_this<UserSession> {
+	using ErrorCode = boost::system::error_code;
+	using tcp = boost::asio::ip::tcp;
 
-
-
-class UserSession : public boost::enable_shared_from_this<UserSession> {
 public:
-	UserSession(boost::asio::io_service& io_service, boost::shared_ptr<ofstream> file);
+	UserSession(boost::asio::io_service& io_service, boost::shared_ptr<std::ofstream> file);
 
-	tcp::socket& get_client_socket();
+	const tcp::socket& get_client_socket() const noexcept;
+	tcp::socket& get_client_socket() noexcept;
 
-	void start(const std::string& proxy_ip, std::size_t proxy_port);
+	void start(const std::string& proxy_ip, short proxy_port) noexcept;
 
-	void handle_connect(const boost::system::error_code& error);
+	void handle_connect(const ErrorCode& error) noexcept;
 
-	void handle_client_read(const boost::system::error_code& error, const std::size_t bytes_transferred);
+	void handle_client_read(const ErrorCode& error, const std::size_t bytes_transferred) noexcept;
 
-	void handle_proxy_write(const boost::system::error_code& error);
+	void handle_proxy_write(const ErrorCode& error) noexcept;
 
-	void handle_proxy_read(const boost::system::error_code& error, const std::size_t bytes_transferred);
+	void handle_proxy_read(const ErrorCode& error, const std::size_t bytes_transferred) noexcept;
 
-	void handle_client_write(const boost::system::error_code& error);
+	void handle_client_write(const ErrorCode& error) noexcept;
 
-	void close();
+	void close() noexcept;
 
 private:
 	enum { default_buffer_size = 8192 };
@@ -49,7 +42,7 @@ private:
 	tcp::socket _proxy_socket;
 	char _client_buffer[default_buffer_size];
 	char _proxy_buffer[default_buffer_size];
-	boost::shared_ptr<ofstream> _file_log;
+	boost::shared_ptr<std::ofstream> _file_log;
 };
 
 #endif // !_USER_SESSION__
